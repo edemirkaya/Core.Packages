12e44e847225b819cf14c2c84d6de2467340039b
Ertugrul DEMIRKAYA <ertugrulldemirkaya@gmail.com>
2024-09-09 00:28:30 +0300

    GetListByDynamicAsync fnc added

--- Core.Persistence/Repositories/IAsyncRepository.cs
+++ Core.Persistence/Repositories/IAsyncRepository.cs
@@ -15,7 +15,7 @@ public interface IAsyncRepository<TEntity, TEntityId>
         );
 
     Task<IPaginate<TEntity>> GetListAsync(
-        Expression<Func<TEntity, bool>> predicate = null,
+        Expression<Func<TEntity, bool>>? predicate = null,
         Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null,
         Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null,
         int index = 0,
@@ -24,6 +24,18 @@ public interface IAsyncRepository<TEntity, TEntityId>
         bool enableTracking = true,
         CancellationToken cancellationToken = default
         );
+
+    Task<IPaginate<TEntity>> GetListByDynamicAsync(
+        DynamicQuery dynamic,
+       Expression<Func<TEntity, bool>>? predicate = null,
+       Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null,
+       int index = 0,
+       int size = 10,
+       bool withDeleted = false,
+       bool enableTracking = true,
+       CancellationToken cancellationToken = default
+       );
+
     Task<bool> AnyAsync(
            Expression<Func<TEntity, bool>>? predicate = null,
            bool withDeleted = false,
