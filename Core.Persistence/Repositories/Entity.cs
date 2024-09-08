19ef030fe435672bc912354ceb3dacaef1414816
Ertugrul DEMIRKAYA <ertugrulldemirkaya@gmail.com>
2024-09-08 18:19:49 +0300

    Add project files.

---
+++ Core.Persistence/Repositories/Entity.cs
@@ -0,0 +1,20 @@
+﻿namespace Core.Persistence.Repositories;
+
+public class Entity<TId>
+{
+    public TId Id { get; set; }
+    public bool IsActive { get; set; }
+    public DateTime CreateDate { get; set; }
+    public DateTime? UpdateDate { get; set; }
+    public DateTime? DeleteDate { get; set; }
+
+    public Entity()
+    {
+        Id = default;
+
+    }
+    public Entity(TId id)
+    {
+        Id = id;
+    }
+}
